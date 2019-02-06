c57f1287be841e9bad192848c5ec79fa23303211
Zaina Cruz-King <zainacruz-king@zipcoders-MacBook-Pro.local>
2019-02-05 19:08:49 -0500

    new changes to project

--- Main.java
+++ Main.java
@@ -1,9 +1,31 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        
+        System.out.println("Enter a name");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String name = scanner.nextLine();
+        
+        if (name.equals("Alice") || name.equals("Bob")) {
+            System.out.println("Hello " + name + ".");
+        }
+        
+       
+       
+   
+        
+        
+       
+        
+        
+   
 
+        
     }
 }
